d21cd5995d92662fea3082971cf575b19dd7a50b
Kamil Plich <kamilplich2@gmail.com>
2020-07-12 17:50:42 +0200

    refactor: delete dummy classes
    Signed-off-by: Kamil Plich <kamilplich2@gmail.com>

--- src/main/kotlin/kplich/backend/repositories/DummyChildRepository.kt
+++
@@ -1,6 +0,0 @@
-package kplich.backend.repositories
-
-import kplich.backend.entities.DummyChild
-import org.springframework.data.repository.CrudRepository
-
-interface DummyChildRepository : CrudRepository<DummyChild, Long>
