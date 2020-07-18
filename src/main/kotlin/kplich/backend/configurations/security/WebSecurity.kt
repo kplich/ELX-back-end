ecf57908a10d15b41d1009b912c2ba67a4f9c2e7
Kamil Plich <kamilplich2@gmail.com>
2020-07-18 20:14:29 +0200

    fix: set-up cors
    Signed-off-by: Kamil Plich <kamilplich2@gmail.com>

--- src/main/kotlin/kplich/backend/configurations/security/WebSecurity.kt
+++ src/main/kotlin/kplich/backend/configurations/security/WebSecurity.kt
@@ -1,8 +1,10 @@
 package kplich.backend.configurations.security
 
 import kplich.backend.services.UserDetailsServiceImpl
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
@@ -12,12 +14,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 class WebSecurity(
         private val userDetailsService: UserDetailsServiceImpl) : WebSecurityConfigurerAdapter() {
+
+    @Value("\${cors.origins}")
+    private val allowedOrigins: List<String> = emptyList()
 
     @Throws(Exception::class)
     public override fun configure(authenticationManagerBuilder: AuthenticationManagerBuilder) {
@@ -30,10 +38,13 @@ class WebSecurity(
     }
 
     override fun configure(http: HttpSecurity) {
-        http.csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/auth/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
+        http
+                .cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
                 .addFilter(JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(JWTAuthorizationFilter(authenticationManager()))
     }
@@ -44,4 +55,17 @@ class WebSecurity(
         return super.authenticationManagerBean()
     }
 
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val configuration = CorsConfiguration().apply {
+            allowedOrigins = this@WebSecurity.allowedOrigins
+            allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+            allowedHeaders = listOf(HttpHeaders.CONTENT_TYPE)
+        }
+        println(configuration.allowedOrigins)
+
+        return UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", configuration)
+        }
+    }
 }
