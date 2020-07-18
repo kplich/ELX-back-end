231342860a2fb7606d17b23170711985dccf9843
Kamil Plich <kamilplich2@gmail.com>
2020-07-18 15:05:59 +0200

    refactor: move configuration to application profiles
    Signed-off-by: Kamil Plich <kamilplich2@gmail.com>

--- src/main/kotlin/kplich/backend/configurations/security/JwtUtil.kt
+++ src/main/kotlin/kplich/backend/configurations/security/JwtUtil.kt
@@ -2,8 +2,6 @@ package kplich.backend.configurations.security
 
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.MalformedJwtException
-import io.jsonwebtoken.UnsupportedJwtException
 import io.jsonwebtoken.security.Keys
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
@@ -26,27 +24,6 @@ class JwtUtil : Serializable {
                 .setIssuedAt(Date())
                 .signWith(key)
                 .compact()
-    }
-
-    fun getUserNameFromJwtToken(token: String): String {
-        return Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(token).body.subject
-    }
-
-    fun validateJwtToken(authToken: String): Boolean {
-        try {
-            val key = Keys.hmacShaKeyFor(jwtSecret.toByteArray())
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(authToken)
-            return true
-        } catch (e: SecurityException) {
-            logger.error("Invalid JWT signature: ${e.message}")
-        } catch (e: MalformedJwtException) {
-            logger.error("Invalid JWT token: ${e.message}")
-        } catch (e: UnsupportedJwtException) {
-            logger.error("JWT token is unsupported: ${e.message}")
-        } catch (e: IllegalArgumentException) {
-            logger.error("JWT claims string is empty: ${e.message}")
-        }
-        return false
     }
 
     fun parseJwtClaims(token: String): Claims {
