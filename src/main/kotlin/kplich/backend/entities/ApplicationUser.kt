97a9ab28903f51811709f6786acff9cb96acde52
Kamil Plich <kamilplich2@gmail.com>
2020-08-06 22:52:59 +0200

    fix: wrong column name
    Signed-off-by: Kamil Plich <kamilplich2@gmail.com>

--- src/main/kotlin/kplich/backend/entities/ApplicationUser.kt
+++ src/main/kotlin/kplich/backend/entities/ApplicationUser.kt
@@ -25,7 +25,7 @@ data class ApplicationUser(
         @ManyToMany(fetch = FetchType.LAZY)
         @JoinTable(
                 name = "user_roles",
-                joinColumns = [JoinColumn(name = "id")],
+                joinColumns = [JoinColumn(name = "user_id")],
                 inverseJoinColumns = [JoinColumn(name = "role_id")])
         var roles: MutableSet<Role> = hashSetOf()
 )
